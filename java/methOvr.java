af0cf39173511a6c8afaa2cfc32462fd42d6a7b3
Hawk1509 <129725289+Hawk1509@users.noreply.github.com>
2023-11-03 14:30:10 +0530

    method overloading
    program to calculate the area of the circle rectangle and square by using method overloading

---
+++ java/methOvr.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class AreaCalc
+{
+	double a;
+	void area(int l,int b)
+	{
+		a=l*b;
+		System.out.println("Area of the rectangle is "+ a);
+	}
+	void area(int i)
+	{
+		a=i*i;
+		System.out.println("Area of the square is "+ a);
+	}
+	void area(double r)
+	{
+		a=3.14*r;
+		System.out.println("Area of the circle is "+ a);
+	}
+}
+class Area
+{
+	public static void main(String arg[])
+	{
+		Scanner sc=new Scanner(System.in);
+		AreaCalc myRec=new AreaCalc();
+		AreaCalc mySq=new AreaCalc();
+		AreaCalc myCir=new AreaCalc();
+		myRec.area(7,8);
+		mySq.area(5);
+		myCir.area(4);
+	}
+}

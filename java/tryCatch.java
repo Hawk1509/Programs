62c41ba9985adefbd91274a847225be8fe8af84b
Hawk1509 <129725289+Hawk1509@users.noreply.github.com>
2023-11-20 16:17:12 +0530

    exception handling

--- java/tryCatch.java
+++ java/tryCatch.java
@@ -1,18 +1,31 @@
-//program to illustrate the use of try, catch, throw, throws, finally
-class tryCatch
-{
-	public static void main(String [] args)
-	{
+import java.io.IOException;
+class tryCatch{
+	public static void main(String[] args)throws IOException{
 		try
 		{
-			int a = 10;
-			int c = a/0;
-			System.out.println(c);
+			int sum=50/0;
 		}
 		catch(ArithmeticException e)
 		{
-			System.out.println(e);
+			System.out.println("EXCEPTION CAUGHT");
 		}
-		System.out.println("Rest of the code");
+		finally
+		{
+			System.out.println("FINALLY AFTER HANDLING");
+		}
+		int a=1;
+		if(a==1)
+		{
+			throw new IOException("HANDLING USING THROW");
+		}
 	}
 }
+/*
+OUTPUT
+EXCEPTION CAUGHT
+FINALLY AFTER HANDLING
+Exception in thread "main" java.io.IOException: HANDLING USING THROW
+	at tryCatch.main(tryCatch.java:19)
+*/
+
+

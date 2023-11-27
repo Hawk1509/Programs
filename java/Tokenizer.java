8cee6572d81ce1adb64b1ca2a00270e01080517d
Hawk1509 <129725289+Hawk1509@users.noreply.github.com>
2023-11-27 14:50:09 +0530

    reading integer using tokenizer

---
+++ java/Tokenizer.java
@@ -0,0 +1,13 @@
+//program to read line of intergers and display each integer and sum of all integers
+import java util.*;
+class Tokenizer
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of digits: ");
+		String digit = sc.nextLine();
+		String Tokenizer token = new String Tokenizer
+		 
+	}
+}
